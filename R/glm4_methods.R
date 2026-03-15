61ec3e2cebd2963e98e7780bae8fbd1386627374
awhug <angus-hughes@outlook.com>
2026-03-15 21:12:07 +1030

    Replace stats:::format.perc (removed)

--- R/glm4_methods.R
+++ R/glm4_methods.R
@@ -55,7 +55,8 @@ confint.glm4 <- function (object, parm, level = 0.95, ...)
 		parm <- pnames[parm]
 	a <- (1 - level)/2
 	a <- c(a, 1 - a)
-	pct <- stats:::format.perc(a, 3)
+	#pct <- stats:::format.perc(a, 3)
+	pct <- paste(format(100 * a, trim = TRUE, scientific = FALSE, digits = 3),"%")
 	fac <- qnorm(a)
 	ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm, pct))
 	ses <- sqrt(Matrix::diag(vcov(object)))[parm]
